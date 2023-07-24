c73ce61d390c8413efc636b557642339b5a3525a
dbknguyen <doanbakhanhnguyen7@gmail.com>
2023-07-24 18:31:31 +0700

    Commit changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import headerVideo from "./assets/pexels-pixabay-855204-1920x1080-30fps.mp4";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <video className='header-video' src={headerVideo} muted autoPlay loop></video>
+        <h1 className="main-text">InitiaRe</h1>
+        <div className="clock-wrap">
+          <div className="hour-mark"></div>
+          <div className="hour-mark"></div>
+          <div className="hour-mark"></div>
+          <div className="hour-mark"></div>
+          <div className="hour-mark"></div>
+          <div className="hour-mark"></div>
+          <div className="long-hand"></div>
+          <div className="short-hand"></div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
